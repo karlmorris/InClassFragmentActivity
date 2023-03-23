c4f6098127651f5739cb9a2ecce0a08fc087b8ed
Dat Nguyen <tuj81499@temple.edu>
2023-03-23 12:17:02 -0400

    Add mainViewModel and use lazy on ImageDisplayFragment.kt

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,21 +2,26 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
 
+        mainViewModel.setImageIds(imageArray)
+
         // Attach an instance of ImageDisplayFragment using factory method
-        val fragment = ImageDisplayFragment.newInstance(imageArray)
+        //val fragment = ImageDisplayFragment.newInstance(imageArray)
         val fragmentT = supportFragmentManager.beginTransaction()
-        fragmentT.add(R.id.fragmentContainerView, fragment)
+        fragmentT.add(R.id.fragmentContainerView, ImageDisplayFragment())
         fragmentT.commit()
     }
 }
