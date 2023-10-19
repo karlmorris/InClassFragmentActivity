c191461c46fa0553c9ee421349c60f45fe722405
aliasgersg <alighotawala02@gmail.com>
2023-10-19 11:49:01 -0400

    In this commit attached an instance of ImageDisplayFragment using factory method

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
+        val imageDisplayFragment = ImageDisplayFragment.newInstance(imageArray)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, imageDisplayFragment)
+            .commit()
 
     }
 }
