4de97ee04522d4a7bd40f5a74d60eae63c7194d2
Jun Zhou <tun40202@temple.edu>
2023-10-19 11:45:18 -0400

    add the newinstance function

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,11 @@ class MainActivity : AppCompatActivity() {
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
-
-        // Attach an instance of ImageDisplayFragment using factory method
+        val fragment1 = ImageDisplayFragment.newInstance(imageArray)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView, fragment1)
+            .commit()
 
     }
 }
